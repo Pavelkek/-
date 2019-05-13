413688586c76aed58e5e3c802789ba5133f47925
Samotaev Pavel Andreevich <p.samotaev@tcsbank.ru>
2019-05-13 13:57:02 +0300

    task6IsCompleted

---
+++ Задания/Массивы.js
@@ -0,0 +1,27 @@
+// 1. Перебор массива с выводом в консоль каждого элемента
+var arr = [];
+for(let i = 0; i < 10; i++) {
+    arr.push(i);
+    console.log(arr[i]);
+}
+
+// 2. Преобразовать массив в строку и обратно в массив
+var strFromArr = "";
+strFromArr += arrjoin(" ");
+console.log(strFromArr);
+var arrNew = strFromArr.split(' ');
+for(let i = 0; i < arrNew.length; i++) {
+    console.log(arrNew[i]);
+}
+
+// 3. Добавить к каждому элементу массива слово hello
+for(let i = 0; i < arrNew.length; i++) {
+    arrNew[i] = arrNew[i] + "hello";
+    console.log(arrNew[i]);
+}
+
+// 4. Преобразовать числовой массив в bollean 
+for(let i = 0; i < arr.length; i++) {
+    arr[i] = !!arr[i];
+    console.log(arr[i]);
+}
