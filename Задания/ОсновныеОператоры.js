81f3b6872f2f30868090e78e61a367366fbb3183
Samotaev Pavel Andreevich <p.samotaev@tcsbank.ru>
2019-05-13 12:11:45 +0300

    task3IsCompleted

---
+++ Задания/ОсновныеОператоры.js
@@ -0,0 +1,18 @@
+var a = 1;
+a = -a;
+console.log(a);
+var b = "2";
+console.log(+b);
+console.log(a + +b);
+var c, d, e;
+c = d = e = 4 * 4;
+console.log(5 % 2);
+console.log(++a)
+console.log(--b);
+var f = true;
+var g = false;
+console.log(f || g);
+console.log(f && g);
+console.log(c += d);
+var h = (2,"str");
+console.log(h);
