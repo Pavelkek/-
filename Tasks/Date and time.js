ab0df63f659a4f182ea3be3339478fffea4bcdb7
Samotaev Pavel Andreevich <p.samotaev@tcsbank.ru>
2019-05-15 11:30:43 +0300

    task10IsCompleted

--- Tasks/Date and time.js
+++ Tasks/Date and time.js
@@ -16,59 +16,78 @@ console.log(date);
 
 // Задача 2
 function getWeekDay(date) {
-    var carrentDate = date;
+    var currentDate = date;
     var options = {
-        weekday: "long",
+        weekday: "short",
     };
-    switch( carrentDate.toLocaleString("ru", options) ) {
-        case "понедельник":
-            return "пн";
-        case "вторник":
-            return "вт";
-        case "среда":
-            return "ср";
-        case "четверг":
-            return "чт";
-        case "пятница":
-            return "пт";
-        case "суббота":
-            return "сб";
-        case "воскресенье":
-            return "вс";
-        default:
-            return "нет ткаого дня недели";
-    }
+    return currentDate.toLocaleString("ru", options);
 }
 
 // Задача 3
 function getLocalDay(date) {
-    var carrentDate = date;
-    var options = {
-        weekday: "long",
-    };
-    switch( carrentDate.toLocaleString("eu", options) ) {
-        case "Mon":
-            return "1";
-        case "Tue":
-            return "2";
-        case "Wed":
-            return "3";
-        case "Thu":
-            return "4";
-        case "Fri":
-            return "5";
-        case "Sat":
-            return "6";
-        case "Sun":
-            return "7";
-        default:
-            return "нет ткаого дня недели";
-    }
+    var currentDate = date;
+    return currentDate.getDay();
 }
 
-// Задача 4 (не доделано)
-function getDateAgo (date, days) {
+// Задача 4
+function getDateAgo(date, days) {
     var newDate = new Date(date);
-    newDate.setDate( newDate.getDay() - days );
-    return newDate;
+    newDate.setDate( newDate.getDate() - days );
+    return newDate.getDate();
+}
+
+// Задача 5
+function getLastDayOfMonth(year, month) {
+    var date = new Date (year, month + 1);
+    date.setDate(date.getDate() - 1);
+    return date.getDate();
+}
+
+// Задача 6
+function getSecondsToday() {
+    var date = new Date();
+    var currentDate = new Date();
+    date.setHours(0, 0, 0, 0);
+    return  Math.round( (+currentDate - +date) / 1000 );
+}
+
+// Задача 7
+function getSecondsToTommorrow() {
+    var tommorrowDate = new Date();
+    var currentDate = new Date();
+    tommorrowDate.setDate( tommorrowDate.getDate() + 1 );
+    tommorrowDate.setHours(0, 0, 0, 0);
+    return  Math.round( (+tommorrowDate - +currentDate) / 1000 );
+}
+
+// Задача 8
+function formatDate(date) {
+    var currentDate = date;
+    var options = {
+        year: "2-digit",
+        month: "2-digit",
+        day: "2-digit"
+    }
+    return currentDate.toLocaleString("ru", options);
+}
+
+// Задача 9
+function formatDateTwo(date) {
+    var currentDate = new Date();
+    var options = {
+        year: "2-digit",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit"
+    }
+    if (+currentDate - +date < 1 * 1000) {
+        return "толко что";
+    } else if (+currentDate - +date < 59 * 1000) {
+        return (+currentDate - +date) / 1000 + " сек. назад";
+    } else if (+currentDate - +date < 59 * 1000 * 60) {
+        return (+currentDate - +date) / 1000 / 60 + " мин. назад";
+    } else {
+        return date.toLocaleString("ru", options);
+    }
 }
